723b1bd7647d0e764fc08cfdc1b183e731379ec9
Fabien Di Tore <fabien@ditore.ch>
2017-01-18 16:27:13 +0100

    ok

---
+++ src/response_fabien-ThinkPad-T420s_Jan-18-162700-2017_Conflict/Response.php
@@ -0,0 +1,27 @@
+<?php
+namespace FDT2k\ICE\CORE\Response;
+
+class Response extends  \FDT2k\ICE\CORE\IObject{
+
+	var $mime = 'text/html';
+	#public $response_code = 200;
+	function __construct($buffer=''){
+		$this->setResponseCode(200);
+		$this->buffer= $buffer;
+	}
+
+
+	function output_headers(){
+		header('HTTP/1.0 '.$this->getResponseCode());
+		header('Content-type :'.$this->mime);
+
+	}
+
+	function output(){
+		$this->output_headers();
+		echo $this->buffer;
+	}
+
+
+
+}
