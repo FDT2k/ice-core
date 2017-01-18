cdc28fcdbe0a6333b103bd4ca96b9f644fe01f39
Fabien Di Tore <fabien@ditore.ch>
2017-01-18 16:06:32 +0100

    ok

--- src/Env.php
+++ src/Env.php
@@ -34,7 +34,7 @@ class Env{
 
 
 	public static function preinit($argv){
-//		spl_autoload_register(__NAMESPACE__ .'\Env::autoload');
+		spl_autoload_register(__NAMESPACE__ .'FDT2k\ICE\CORE\Env::autoload');
 		self::$profiler = new Profiler;
 
 		//Doing some php configuration
