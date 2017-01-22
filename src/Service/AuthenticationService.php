30d29aa24ef55706abe87923e13f1f166a64680f
Fabien Di Tore <fabien@ditore.ch>
2017-01-22 12:03:59 +0100

    ok

--- src/Service/AuthenticationService.php
+++ src/Service/AuthenticationService.php
@@ -4,7 +4,7 @@ namespace FDT2k\ICE\CORE\Service;
 
 use \FDT2k\ICE\CORE as ROOT
 
-class AuthenticationService extends ROOT\IObject {
+class AuthenticationService extends \ROOT\IObject {
 
 //	abstract public function authenticate($login,$password,$opts);
 //  abstract public function is_logged();
