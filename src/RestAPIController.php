0e8df10eb02d629dac21221988f0f1bf08302e85
Fabien Di Tore <fabien@ditore.ch>
2017-02-08 17:14:49 +0100

    fixed Rest Controller

--- src/RestAPIController.php
+++ src/RestAPIController.php
@@ -3,13 +3,7 @@ namespace FDT2k\Noctis\Core;
 
 class RestAPIModule extends Controller{
 
-	function getModelUser(){
-		return new \ICE\core\model\JWTUser();
-	}
 
-	function getModel(){
-		return new \base\model\Database();
-	}
 
 	function publicAccess(){
 		return Env::getRequest()->getMethod() == "OPTIONS" ||$this->action == "_authenticate" || $this->action == '_register';
@@ -17,44 +11,35 @@ class RestAPIModule extends Controller{
 
 	function beforeActionRun($action){
 
+			$this->is_logged = Env::getAuthService()->is_logged();
 
-			$token = Env::getRequest()->getToken();
-//			var_dump($token);
-			$logged= $this->user->loginJWT($token);
-			$this->response->data["token"]=$token;
-			if(!$logged && !$this->publicAccess()){
-			//	$this->response->setResponseCode(401);
-				$this->response->setError($this->user->getError(),$this->user->getErrorCode());
-			}/*else if($this->user->willExpireSoon($token)){
-				//send a new token;
-				//$this->response->
-			}*/
-
-	}
-
-	function _authenticateAction(){ // token generation
-		#$this->response->data="test";
-		if(Env::getRequest()->getMethod() == "POST"){
-			$this->response->setError("Failed");
-
-			#$this->response->data=$this->request->get('username');
-			if($this->user->login($this->request->get('username'),sha1($this->request->get('password')))){
-
-				$this->response->clearError();
-				$this->response->data["token"] = $this->user->getToken();
+			if(!$this->is_logged && !$this->publicAccess()){
+				$this->response->setResponseCode(401);
+				$this->response->setError("unauthorized access");
 			}
 
-		}
-		return $this->response;
 	}
 
-	function allowMethod($array){
+	function initResponse(){
+		$this->response = new Response\JSONResponse();
+	}
+
+
+
+	function assertMethodAllowed($array){
 		if (!in_array(Env::getRequest()->getMethod(), $array) && Env::getRequest()->getMethod() != "OPTIONS"){
 			$this->response->setResponseCode(405);
 			$this->response->setError("Method Not Allowed");
 			return false;
 		}
 		return true;
+	}
+
+	function authenticateAction(){
+		if(!Env::getAuthService()->authenticate($name,$password)){
+			$this->response->setError("user / password is not valid");
+		}
+		return $this->response;
 	}
 
 }
